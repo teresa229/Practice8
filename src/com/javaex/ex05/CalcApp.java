39f6b1d1666c1c39c25ae99289ea6515469a60a4
teresa229 <scarlett_29@naver.com>
2020-12-07 23:50:54 +0900

    1st

---
+++ src/com/javaex/ex05/CalcApp.java
@@ -0,0 +1,30 @@
+package com.javaex.ex05;
+
+import java.util.Scanner;
+
+public class CalcApp {
+
+    public static void main(String[] args) {
+    	
+    	Scanner sc = new Scanner(system.in);
+
+    	while(run) {
+    		System.out.print(">>");
+    		
+    		int num01 = sc.nextInt();
+    		int num02 = sc.nextInt();
+    		
+    		swich(num){
+    			
+    			case sum:
+    				System.out.print(">>");
+    				num
+    				
+    			
+    			
+    			
+    		}
+    		
+    	}
+    }
+}
---
+++ src/com/javaex/ex07/Point.java
@@ -0,0 +1,34 @@
+package com.javaex.ex07;
+
+public class Point {
+	
+	private int x;
+	private int y;
+	
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	//equals() 메소드를 작성하세요.
+	public boolean equals(Object obj) {
+		if(this.hashCode() == ((Point)obj).hashCode()) {
+			return true;
+		}else {
+			if(this.y == ((Point)obj).y){
+				if(this.x == ((Point)obj).x) {
+					return false;
+				}else {
+					return false;
+				}
+			}else {
+				if(this.x == ((Point)obj).x) {
+					return true;
+				}else {
+					return false;
+				}
+			}
+		}
+	}
+	
+}
